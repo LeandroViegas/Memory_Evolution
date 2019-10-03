f1d45395325a68779e422c150387a074f537b2b5
Leandro Alves Viegas <L3androViegas@gmail.com>
2019-10-03 14:27:32 -0300

    Memory_Evolution
    V1.1.2

---
+++ Memory Evolution/Assets/Scripts/Scenes Controllers/EntradaController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntradaController : MonoBehaviour {
+
+    public class Falas{
+        public string personagem;
+        public string fala;
+    }
+
+    public Falas[] falas;
+    // Use this for initialization
+	void Start () {
+        falas[0].personagem = "izuna";
+        falas[0].fala = "Bom dia meu caro";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    		
+
+	}
+
+    public void Falar(int fala)
+    {
+        if (!FindObjectOfType<talks>().falando)
+            FindObjectOfType<talks>().Falar(falas[fala].fala);
+    }
+}
---
+++ Memory Evolution/Assets/Scripts/Scenes Controllers/talks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class talks : MonoBehaviour {
+
+    string frase;
+    public bool falando = false;
+    float pos = 0;
+    public GameObject txt;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Up4date is called once per frame
+	void Update () {
+        if (falando)
+        {
+            if (Convert.ToInt32(pos) < frase.Length)
+                pos += Time.deltaTime * 20f;
+            else
+                falando = false;
+            txt.GetComponent<UnityEngine.UI.Text>().text = frase.ToString().Substring(0, Convert.ToInt32(pos));
+        }
+        
+    }
+
+    public void Falar(string fala)
+    {
+        frase = fala;
+        falando = true;
+    }
+}
